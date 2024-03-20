1b554826ebbff77e933db8a88bcaef2f20498a1c
F4r3t <sultangareevalan@gmail.com>
2024-03-20 21:17:21 +0300

    My First Test in Java)

---
+++ Java_main_testing/src/main/java/org/example/SimpleTest.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+public class SimpleTest {
+    @Test
+    public void FirstTest() {
+        assertTrue(true);
+    }
+    public void main(){
+        FirstTest();
+    }
+}
